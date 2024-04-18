23946967893960e9bfe38b5c39a4f9c45d988905
Juliacruz97 <juliacruz97@hotmail.com>
2024-04-18 11:18:12 -0300

    c4

--- Weekly Tasks 02/bank.py
+++ Weekly Tasks 02/bank.py
@@ -1,9 +1,6 @@
-'The program should: 1) Prompt the user and read in two money amounts (in cent) 
-' 2) Add the two amounts
-'3)Print out the answer in a human readable format with a euro sign and 
-'decimal point between the euro and cent of the amount 
 
-num1= int(input('Enter amount1 : '))
-num2= int(input('Enter amount 2: '))
+n1= float(input('Enter amount1 : '))
+n2= float(input('Enter amount 2: '))
 
-print (num1)
+total = float (n1+n2)
+print ('the sum of these is €{:..2}', total)
