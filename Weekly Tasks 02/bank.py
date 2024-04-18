fd75da05e9d821b16ee9633ced563c602379e754
Juliacruz97 <juliacruz97@hotmail.com>
2024-04-18 11:38:29 -0300

    c5

--- Weekly Tasks 02/bank.py
+++ Weekly Tasks 02/bank.py
@@ -1,6 +1,6 @@
 
-n1= float(input('Enter amount1 : '))
-n2= float(input('Enter amount 2: '))
+n1= float(input('Enter amount1 : € '))
+n2= float(input('Enter amount 2: € '))
 
 total = float (n1+n2)
-print ('the sum of these is €{:..2}', total)
+print('The total sum of these is € {:.2f}'.format(total))
