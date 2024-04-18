d0cce504f008904a70b3fcc4ff8a2c116bca2df5
Juliacruz97 <juliacruz97@hotmail.com>
2024-04-18 10:45:29 -0300

    c3

---
+++ Weekly Tasks 02/bank.py
@@ -0,0 +1,9 @@
+'The program should: 1) Prompt the user and read in two money amounts (in cent) 
+' 2) Add the two amounts
+'3)Print out the answer in a human readable format with a euro sign and 
+'decimal point between the euro and cent of the amount 
+
+num1= int(input('Enter amount1 : '))
+num2= int(input('Enter amount 2: '))
+
+print (num1)
