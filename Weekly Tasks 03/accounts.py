81b6c70324681b853085a5a67c54b9b457947108
Juliacruz97 <juliacruz97@hotmail.com>
2024-04-18 12:46:21 -0300

    C7

---
+++ Weekly Tasks 03/accounts.py
@@ -0,0 +1,2 @@
+account_number = (input('Please enter an 10 digit account number: '))[0:10]
+print('XXX-XXX-',account_number [-4:])
---
+++ Weekly Tasks 03/accounts_0.py
@@ -0,0 +1,2 @@
+account_number = (input('Please enter an 10 digit account number: '))
+print('XXX-XXX-',account_number [-4:])
