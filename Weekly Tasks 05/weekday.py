f98935bb147cd3778fd499a44abf5dc858501777
Juliacruz97 <juliacruz97@hotmail.com>
2024-04-19 10:42:59 -0300

    c12

--- Weekly Tasks 05/weekday.py
+++ Weekly Tasks 05/weekday.py
@@ -1,2 +1,19 @@
-hoje = str(input('Is today a weekday ? which day is today ?'))
-print (hoje)
+from datetime import date
+# Extract today date
+today = date.today()
+
+if today.weekday() == 0:
+    print("Today is: Monday - weekday")
+elif today.weekday() == 1:
+    print("Today is: Tuesday - weekday")
+elif today.weekday() == 2:
+    print("Today is: Wednesday - weekday")
+elif today.weekday() == 3:
+    print("Today is: Thursday - weekday")
+elif today.weekday() == 4:
+    print("Today is: Friday - weekday")
+elif today.weekday() == 5:
+    print("Today is: Saturday - weekend- Huhuhh ")
+else:
+    print("Today is: Sunday - weekend - Huhuhuh")
+
