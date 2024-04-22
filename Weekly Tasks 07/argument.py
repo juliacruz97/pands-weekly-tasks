2c745736bf600d2352b9176158ca74195f9b2006
Juliacruz97 <juliacruz97@hotmail.com>
2024-04-22 09:59:07 -0300

    c18

--- Weekly Tasks 07/argument.py
+++
@@ -1,3 +0,0 @@
-with open("romeo-and-juliet", "r") as arquivo:
-    romeo-and-juliet = arquivo.read()
-    print(romeo-and-juliet)
---
+++ argument.py
@@ -0,0 +1,5 @@
+nDe_vezes = 0
+for letter in romeo-and-juliet :
+    if letter == "b" :
+        nDe_vezes = nDe_vezes +1
+        print (nDe_vezes)
