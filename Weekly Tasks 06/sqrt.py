93cc1e283d010169e79d1c34fe98b6aba3761e7e
Juliacruz97 <juliacruz97@hotmail.com>
2024-04-22 09:35:39 -0300

    c17

--- Weekly Tasks 06/sqrt.py
+++ Weekly Tasks 06/sqrt.py
@@ -1,9 +1,7 @@
 '''Write a program that takes a positive floating-point number as input and outputs an 
 approximation of its square root.'''
 
-
-n= int(input('Please enter a positive number: '))
-n1= n+1
-sqrt= n*n 
-sqrt = n + (n1*n1)/(2*n1)
+num= int(input('Input number: '))
+sqrt = num ** (1/2)
 print(sqrt)
+
