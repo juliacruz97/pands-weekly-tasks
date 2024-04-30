4fed50f110effe5ea04010ca89bb8b8f3978c84d
juliacruz97 <juliacruz97@hotmail.com>
2024-04-30 18:42:19 -0300

    c23

--- Hello World.py
+++
@@ -1,7 +0,0 @@
-"Hello World "
-"Program that print Hello World"
-"Autor Julia Cruz"
-
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
---
+++ helloWorld.py
@@ -0,0 +1,5 @@
+#Hello World "
+# This program just print  Hello World"
+# Autor Julia Cruz"
+
+print('Hello, world!')
