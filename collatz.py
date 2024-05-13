a8150b302cbd3952ff2ac6c58e99c266b36b00e4
juliacruz97 <juliacruz97@hotmail.com>
2024-05-13 10:21:00 -0300

    c24

--- collatz.py
+++ collatz.py
@@ -1,15 +1,23 @@
+# Collatz.py, is a program that asks the user to input any positive integer and 
+# outputs the successive values of the following calculation.
+# At each step calculate the next value by taking the current value and, 
+# if it is even, divide it by two, but if it is odd, multiply it by three and add one.
+# Have the program end if the current value is one.
+# Author: Julia Cruz
 
-10
-num = int (input('Please enter any positive integer number : '))
-
-
-if num %2==0:
-    num=num/2
-
-
-else:   
-    num %2 !=0
-    num=(num *3)+1
-    num = num
-   
+def collatz (num):
+    if (num % 2==0):
+       return num//2
+    elif (num % 2 == 1):
+       return num * 3 + 10
+    else: 
+        print ('It didnt work')
+        return None  
+print ('Enter a num.')
+num = (int(input()))
+print('Collatz Sequence:')   
 print(num)
+while (num != 1) :
+  num = collatz(num)
+print (num) 
+print (num)
