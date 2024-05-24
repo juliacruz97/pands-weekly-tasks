c2efdc484d4a23e2c80fc7ccad9eb2e7ea5be2ef
juliacruz97 <juliacruz97@hotmail.com>
2024-05-24 10:23:41 -0300

    24_05T10:23am

--- argument.py
+++ argument.py
@@ -1,7 +1,11 @@
+#argument.py
+#This program reads in a text file and outputs the number of e's 
+# it contains.
+#Author : Julia Cruz
+
+
 file = open ("romeo_and_juliet.txt","r")
-nletter=0
-content = file.read()
-for letter in content:
-    if letter == "b" or "B":
-        nletter = nletter + 1
-print('In romeo_and_juliet there are {} letters b and B '.format(nletter))
+line = file.read
+for item in lines:
+    print(item,end="")
+file.close
--- weekday.py
+++ weekday.py
@@ -6,17 +6,17 @@ from datetime import date
 today = date.today()
 
 if today.weekday() == 0:
-    print("Today is: Monday - weekday")
+    print("Today is: Monday - Yes, unfortunately today is a weekday.")
 elif today.weekday() == 1:
-    print("Today is: Tuesday - weekday")
+    print("Today is: Tuesday - Yes, unfortunately today is a weekday.")
 elif today.weekday() == 2:
-    print("Today is: Wednesday - weekday")
+    print("Today is: Wednesday - Yes, unfortunately today is a weekday.")
 elif today.weekday() == 3:
-    print("Today is: Thursday - weekday")
+    print("Today is: Thursday - Yes, unfortunately today is a weekday.")
 elif today.weekday() == 4:
-    print("Today is: Friday - weekday")
+    print("Today is: Friday - Yes, unfortunately today is a weekday.")
 elif today.weekday() == 5:
-    print("Today is: Saturday - weekend- Huhuhh ")
+    print("Today is: Saturday - weekend- Huhuhh !!!")
 else:
-    print("Today is: Sunday - weekend - Huhuhuh")
+    print("Today is: Sunday - weekend - Huhuhuh!!!")
 
