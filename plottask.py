db2330d7e89f3175ab8f2739f47332bce1476aef
juliacruz97 <juliacruz97@hotmail.com>
2024-06-01 12:37:28 -0300

    01/06/24T12:37pm

--- plottask.py
+++ plottask.py
@@ -1,3 +1,9 @@
+# scope: plottask.py that displays: a histogram of a normal distribution of a 1000 values 
+# with a mean of 5 and standard deviation of 2, and a plot of the function  
+# h(x)=x3 in the range 0 to 10, 
+# plottask.py
+# Author: Julia Cruz
+
 import numpy as np
 import matplotlib.pyplot as plt
 
