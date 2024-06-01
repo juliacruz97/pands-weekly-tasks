d7fd8e17506b5ed4d735b74708bfbc7aeca67d11
juliacruz97 <juliacruz97@hotmail.com>
2024-06-01 12:25:01 -0300

    01/06/24T12:24pm

--- argument.py
+++ argument.py
@@ -1,3 +1,7 @@
+# The program that reads in a text file and outputs the number of e's it contains
+# Argument.py
+# Author: Julia Cruz
+
 file = open ("romeo_and_juliet.txt","r")
 nletter=0
 content = file.read()
