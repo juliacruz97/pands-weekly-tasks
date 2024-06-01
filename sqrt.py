534dc79696f154c0e8938aada1dfd46e3358f2d7
juliacruz97 <juliacruz97@hotmail.com>
2024-06-01 19:36:00 -0300

    01/06/24T19:35pm

--- sqrt.py
+++ sqrt.py
@@ -1,7 +1,16 @@
-#Write a program that takes a positive floating-point number as input and outputs an 
-#approximation of its square root.
+# Write a program that takes a positive floating-point number as input and outputs an
+# approximation of its square root.
 
-n= int(input('Please input number: '))
-sqrt = n ** (1/2)
-print(sqrt)
+# n= int(input('Please input number: '))
+# sqrt = n ** (1/2)
+# print(sqrt)
 
+
+x = float(input('imput the number to get your square root: '))
+n = x*x
+q = x**2 
+
+sr= (n (n-2))
+jsr = (2 * (n-2)**2)
+
+print(sr)
