e6f6a4ce23f70cb4cbc69e98cd0415d71a9f0f08
Julia Cruz <122570436+juliacruz97@users.noreply.github.com>
2024-06-01 06:55:55 -0300

    Add files via upload

--- argument.py
+++ argument.py
@@ -1,11 +1,7 @@
-#argument.py
-#This program reads in a text file and outputs the number of e's 
-# it contains.
-#Author : Julia Cruz
-
-
 file = open ("romeo_and_juliet.txt","r")
-line = file.read
-for item in lines:
-    print(item,end="")
-file.close
+nletter=0
+content = file.read()
+for letter in content:
+    if letter == "b" or "B":
+        nletter = nletter + 1
+print('In romeo_and_juliet there are {} letters b and B '.format(nletter))
